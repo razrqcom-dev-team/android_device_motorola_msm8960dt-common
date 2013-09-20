a8a519122b89cbd0af5706a68e5129d3ec11a7e5
dhacker29 <davidhackerdvm@gmail.com>
2013-09-20 00:05:59 -0500

    Add caf display & media at AU_LINUX_ANDROID_JB_2.5.2.04.02.02.088.016
    This should match up to stock kernel

---
+++ display/Android.mk
@@ -0,0 +1,10 @@
+display-hals := libgralloc libgenlock libcopybit liblight
+display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+    include $(call all-named-subdir-makefiles,$(display-hals))
+else
+ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+    #This is for mako since it doesn't have the QCOM make functions
+    include $(call all-named-subdir-makefiles,$(display-hals))
+endif
+endif
